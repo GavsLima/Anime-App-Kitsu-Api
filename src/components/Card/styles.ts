3a25414aafb144eb4916b1d7de9a83cff2d964ce
gavslima <gabrielvitor.limamatias4@gmail.com>
2022-02-11 16:25:33 -0300

    styles: Add card style

---
+++ src/components/Card/styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components/native";
+
+export const CenteredView = styled.View`
+  flex-grow: 1;
+  align-items: center;
+`;
+
+export const CardView = styled.View`
+    flex: 1;
+    width: 100%;
+    margin-top: 150px;
+    margin-left: 100px;
+    
+`;
+
+export const ImageAnime = styled.Image`
+  width: 310px;
+  height: 500px;
+`;
