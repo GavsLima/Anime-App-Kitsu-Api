119dd74a7ca28387c9b7d770089bc90ddcfa41a9
gavslima <gabrielvitor.limamatias4@gmail.com>
2022-02-11 17:43:48 -0300

    styles: Add Button stylr

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components/native";
+
+export const CenteredView = styled.View`
+  flex: 1;
+  align-items: center;
+`;
+
+export const TouchableAnime = styled.TouchableOpacity`
+    align-items: center;
+    background-color: magenta;
+    border-radius: 20px;
+    width: 250px;
+    padding: 10px;
+    margin-top: 120px;
+`;
+
+export const TextAnime = styled.Text`
+    color: #ffff;
+`;
