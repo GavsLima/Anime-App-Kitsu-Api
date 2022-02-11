7ad940c87bf5884576fe9bb119ce54550071664b
gavslima <gabrielvitor.limamatias4@gmail.com>
2022-02-11 17:39:58 -0300

    fiz: Change the card style

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -1,16 +1,13 @@
 import styled from "styled-components/native";
 
 export const CenteredView = styled.View`
-  flex-grow: 1;
   align-items: center;
 `;
 
 export const CardView = styled.View`
-    flex: 1;
-    width: 100%;
-    margin-top: 150px;
-    margin-left: 100px;
-    
+  flex: 1;
+  width: 100%;
+  margin-left: 100px;
 `;
 
 export const ImageAnime = styled.Image`
