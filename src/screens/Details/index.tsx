09e5eacca9d7d67230fc8ebe3063a63066cad4e3
gavslima <gabrielvitor.limamatias4@gmail.com>
2022-02-12 16:34:02 -0300

    refactor: Change the get api

--- src/screens/Details/index.tsx
+++ src/screens/Details/index.tsx
@@ -1,31 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import { Text } from 'react-native';
-import { Container } from './styles';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { Text } from "react-native";
+import { Container } from "./styles";
+import { useSelector } from "react-redux";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-import { IGlobalAnimeId } from '../../store/modules/AnimeDetails/Types';
-import { IAnime } from '../../types';
-import api from '../../service/api';
+import { IGlobalAnimeId } from "../../store/modules/AnimeDetails/Types";
+import { IAnime } from "../../types";
+import api from "../../service/api";
 
 const Details: React.FC = () => {
-  const animeId = useSelector((state: IGlobalAnimeId) =>  state.anime_id)
-  const [animeDetail, setAnimeDetails] = useState<IAnime>({} as IAnime)
-  
+  const animeId = useSelector((state: IGlobalAnimeId) => state.anime_id);
+
+  const [animeDetail, setAnimeDetails] = useState<IAnime>({} as IAnime);
+
   useEffect(() => {
-    api.get(`/anime?filter[id]=${animeId}`)
+    api.get(`/anime/${animeId}`)
     .then(response => {
-      setAnimeDetails(response.data.data[0])
+      setAnimeDetails(response.data.data)
       console.log("nome do baui: ")
-      console.log(animeDetail.attributes.canonicalTitle)
+      console.log(animeDetail)
     })
     .catch(e => console.log(e))
   }, [])
 
   return (
-      <Container>
-          <Text>{`Details of:  `}</Text>
-      </Container>
+    <Container>
+      <TouchableOpacity onPress={() => {}}>
+        <Text>{`Details of: `}</Text>
+      </TouchableOpacity>
+    </Container>
   );
-}
+};
 
 export default Details;
