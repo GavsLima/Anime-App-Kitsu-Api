799a1b366fc8b5d11fa5975dbf4107e5bc7f63a1
gavslima <gabrielvitor.limamatias4@gmail.com>
2022-02-12 16:34:25 -0300

    fix: Delete the old links

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,18 +3,17 @@ export interface IAnime {
     attributes: {
         canonicalTitle: string,
         posterImage: {
-            tiny: "https://media.kitsu.io/anime/44081/poster_image/tiny-a4a4653b4472cb8924cd5b90e606fab6.jpeg",
-            large: "https://media.kitsu.io/anime/44081/poster_image/large-8638fde4407a117614ad4438a855aa37.jpeg",
-            small: "https://media.kitsu.io/anime/44081/poster_image/small-f3f2a7f24a8d7e2b9a5c73e7aa15908e.jpeg",
-            medium: "https://media.kitsu.io/anime/44081/poster_image/medium-017a80e3ca6176dc689a3b95586b0814.jpeg",
-            original: "https://media.kitsu.io/anime/44081/poster_image/f621d8ffeaf6b1883024c009843de46b.jpg",
+            tiny: string,
+            large: string,
+            small: string,
+            medium: string,
+            original: string,
           },
           coverImage: {
-            tiny: "https://media.kitsu.io/anime/44081/cover_image/tiny-f039c84e130ebb9c0453016e58cc6241.jpeg",
-            large: "https://media.kitsu.io/anime/44081/cover_image/large-a5ef1fa23f686e8f2b11f4d33fe38b92.jpeg",
-            small: "https://media.kitsu.io/anime/44081/cover_image/small-840ec82f0786fef4c1b53f91029c3923.jpeg",
-            original: "https://media.kitsu.io/anime/44081/cover_image/76f0b283fe588a7b0c4782d4c2dc9b99.jpg",
-    
+            tiny: string,
+            large: string,
+            small: string,
+            original: string,
           },
     }
 }
