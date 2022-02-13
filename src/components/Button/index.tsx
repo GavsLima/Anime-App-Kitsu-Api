633cabda2d59cc63345f5d085549b68d271c393a
gavslima <gabrielvitor.limamatias4@gmail.com>
2022-02-13 12:45:38 -0300

    feat: Add the function handlePage in Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,15 +1,19 @@
 import React from "react";
-import { IAnime } from "../../types";
+import { IButtonProps } from "../../types";
 
 import { CenteredView, TouchableAnime, TextAnime } from "./styles";
 
 // import { Container } from './styles';
 
-const ButtonAnime: React.FC<IAnime> = ({ attributes }) => {
+const ButtonAnime: React.FC<IButtonProps> = ({ attributes, handlePage }) => {
   return (
     <CenteredView>
-      <TouchableAnime>
-          <TextAnime>{attributes.canonicalTitle}</TextAnime>
+      <TouchableAnime
+        onPress={() =>
+          handlePage(attributes.id, 'Detail')
+        }
+      >
+        <TextAnime>{attributes.attributes.canonicalTitle}</TextAnime>
       </TouchableAnime>
     </CenteredView>
   );
