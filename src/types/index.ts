8094efc77f1b4c5a4a28a343c4c2fd6b12aa0570
gavslima <gabrielvitor.limamatias4@gmail.com>
2022-02-12 21:27:14 -0300

    feat: Update the interfaces

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,12 +8,107 @@ export interface IAnime {
             small: string,
             medium: string,
             original: string,
+            meta: {
+              dimensions: {
+                tiny: {
+                  width: number,
+                  height: number,
+                },
+                large: {
+                  width: number,
+                  height: number,
+                },
+                small: {
+                  width: number,
+                  height: number,
+                },
+                medium: {
+                  width: number,
+                  height: number,
+                }
+              }
+            }
           },
           coverImage: {
             tiny: string,
             large: string,
             small: string,
             original: string,
+            meta: {
+              dimensions: {
+                tiny: {
+                  width: number,
+                  height: number,
+                },
+                large: {
+                  width: number,
+                  height: number,
+                },
+                small: {
+                  width: number,
+                  height: number,
+                },
+              }
+            }
           },
     }
+};
+
+export interface IAnimeDetails {
+  id: string,
+  attributes: {
+      canonicalTitle: string,
+      description: string,
+      posterImage: {
+          tiny: string,
+          large: string,
+          small: string,
+          medium: string,
+          original: string,
+          meta: {
+            dimensions: {
+              tiny: {
+                width: number,
+                height: number,
+              },
+              large: {
+                width: number,
+                height: number,
+              },
+              small: {
+                width: number,
+                height: number,
+              },
+              medium: {
+                width: number,
+                height: number,
+              }
+            }
+          }
+        },
+        coverImage: {
+          tiny: string,
+          large: string,
+          small: string,
+          original: string,
+          meta: {
+            dimensions: {
+              tiny: {
+                width: number,
+                height: number,
+              },
+              large: {
+                width: number,
+                height: number,
+              },
+              small: {
+                width: number,
+                height: number,
+              },
+            }
+          }
+        },
+      youtubeVideoId: string,
+  }
 }
+
