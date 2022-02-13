63fb9f6231519033b2f7644fbb9a3a1110be0fe2
gavslima <gabrielvitor.limamatias4@gmail.com>
2022-02-12 21:26:34 -0300

    style: Update Details styles

--- src/screens/Details/styles.ts
+++ src/screens/Details/styles.ts
@@ -1,7 +1,32 @@
 import styled from "styled-components/native";
+import { Video } from "expo-av";
+
+export const ScrollAnime = styled.ScrollView`
+  background-color: gray;
+`;
 
 export const Container = styled.SafeAreaView`
   flex-grow: 1;
-  justify-content: center;
   align-items: center;
 `;
+
+export const ImageAnime = styled.Image`
+  margin-top: 30px;
+  margin-bottom: 30px;
+`;
+
+export const TextDetail = styled.Text`
+  color: white;
+  font-weight: bold;
+  font-size: 20px;
+  text-align: center;
+  width: 100%;
+`;
+
+export const TextView = styled.View`
+  align-items: center;
+  width: 90%;
+  margin-bottom: 20px;
+`;
+
+
