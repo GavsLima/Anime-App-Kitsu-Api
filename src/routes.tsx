a4238680e207a2da20a9ed5b26bef02227fae8b8
gavslima <gabrielvitor.limamatias4@gmail.com>
2022-02-14 21:27:27 -0300

    feat: Add the routes theme

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,7 @@
 import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { IGlobalAnime } from "./store/modules/AnimeDetails/Types";
+import { useSelector } from "react-redux";
 
 import React from "react";
 import Home from "./screens/Home";
@@ -9,23 +11,42 @@ import Search from "./screens/Search";
 const Routes: React.FC = () => {
   const Stack = createStackNavigator();
 
+  const animeTitle = useSelector((state: IGlobalAnime) => state.anime_title);
+
+  const MyTheme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: "#FE1487",
+      card: "#FE1487",
+      text: "#FFF",
+    },
+  };
+
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={MyTheme}>
       <Stack.Navigator>
-        <Stack.Screen 
-          name="Home" 
-          component={Home} 
+        <Stack.Screen
+          name="Home"
+          component={Home}
           options={{
-            headerShown: false
-          }}  
+            headerShown: false,
+            title: 'Back'
+          }}
         />
-        <Stack.Screen name="Detail" component={Details} />
-        <Stack.Screen 
-          name="Search" 
-          component={Search} 
+        <Stack.Screen
+          name="Detail"
+          component={Details}
+          options={{ 
+            title: animeTitle 
+          }}
+        />
+        <Stack.Screen
+          name="Search"
+          component={Search}
           options={{
-            headerShown: false
-          }}  
+            headerShown: false,
+          }}
         />
       </Stack.Navigator>
     </NavigationContainer>
